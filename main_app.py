0d98f2df0828359c9b98006ec8286ba20794caf2
Philip Housden <philip.houden.04@gmail.com>
2024-12-18 19:12:46 +0700

    initial Commit

---
+++ main_app.py
@@ -0,0 +1,8 @@
+import os
+import shutil
+from pathlib import Path
+import json
+from datetime import datetime
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
