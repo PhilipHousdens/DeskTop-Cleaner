4942854e0549a12c2c6bdde6531f67d17887c259
Philip Housden <philip.houden.04@gmail.com>
2024-12-18 19:13:32 +0700

    Loading Config file

--- main_app.py
+++ main_app.py
@@ -6,3 +6,8 @@ from datetime import datetime
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
+def load_config(config_file="config.json"):
+    with open(config_file, "r") as file:
+        return json.load(file)
+
+FILE_TYPE_FOLDERS = load_config()
